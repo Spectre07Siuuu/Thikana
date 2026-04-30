f6f7f8e488cb780f089d7b6c53c383acb7e39e1c
Sazzad <sazzad11319@gmail.com>
2026-04-30 14:13:38 +0600

    Cleaned up unused boilerplate files, dead components, and large unused assets

--- _install.js
+++
@@ -1,8 +0,0 @@
-import { execSync } from 'child_process'
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const serverDir = join(__dirname, 'server')
-execSync('npm.cmd install multer', { cwd: serverDir, stdio: 'inherit' })
-console.log('Done!')
